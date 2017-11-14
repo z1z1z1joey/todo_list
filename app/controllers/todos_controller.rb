90c0f2a600d03b5cec00084f4ff257b1bc5acfe2
z1z1z1joey <z1z1z1joey@gmail.com>
2017-11-15 00:52:00 +0800

    init

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,44 @@
+class TodosController < ApplicationController
+  before_action :set_todo, :only => [:show, :edit, :update, :destroy]
+  def index
+    @todos = Todo.all
+  end
+  def new
+    @todo = Todo.new
+  end
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to todos_url
+    else
+      render :action => :new
+    end
+  end
+  def show
+    set_todo
+  end
+  def edit
+    set_todo
+  end
+  def update
+    set_todo
+    if @todo.update_attributes(todo_params)
+      redirect_to todos_url
+    else
+      render :action => :edit
+    end
+  end
+  def destroy
+    set_todo
+    @todo.destroy
+
+    redirect_to todos_url
+  end
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :date, :note)
+  end
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+end
